6ff7d6de3d74ea460f1d3ffab38d907b8cbba0bd
Anthony DeVarti <anthony.devarti@gmail.com>
2022-02-09 21:43:16 +0000

    rebuilding

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
+class digitalClock {
+    constructor(element) {
+        this.element = element;
+    }
 
+    getTimeParts(){
+        const now = new date();
+
+        return {
+            hour: now.getHours() % 12 || 12
+            minute: now.getMinutes();
+            isAm: now.getHours < 12
+        };
+    }
+}
+
+const clockElement = document.querySelector('.clock');
+const clockObject = new digitalClock(clockElement);
+
+
+console.log(clockObject.getTimeParts());
